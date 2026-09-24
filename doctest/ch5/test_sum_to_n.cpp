78330aa45f6dc94629498d841004a6cf504554c9
Owen <owentemin@gmail.com>
2026-09-24 09:19:38 -0400

    setting uptest case

---
+++ doctest/ch5/test_sum_to_n.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+using namespace std;
+
+TEST_CASE("sum_to_n(int n) returns sum of integers from 1 to n") {
+    CHECK(sum_to_n(3) == 6);
+    CHECK(sum_to_n(7) == 28);
+    CHECK(sum_to_n(1) == 1);
+    CHECK(sum_to_n(42) == 903);
+}

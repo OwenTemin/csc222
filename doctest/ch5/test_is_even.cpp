57d161018fee24433054d2bff1aa21d952a762bc
Owen <owentemin@gmail.com>
2026-09-24 08:25:32 -0400

    making test cases for even test

---
+++ doctest/ch5/test_is_even.cpp
@@ -0,0 +1,11 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+using namespace std;
+
+TEST_CASE("is_even identifies even numbers") {
+    CHECK(is_even(0) == true);
+    CHECK(is_even(2) == true);
+    CHECK(is_even(-4) == true);
+    CHECK(is_even(1) == false);
+    CHECK(is_even(-7) == false);
+}

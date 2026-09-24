61fdeaecb766b5a322ff6a9057d8f817e3e1c6c9
Owen <owentemin@gmail.com>
2026-09-24 08:55:53 -0400

    adding test for find largest

---
+++ doctest/ch5/test_find_largest.cpp
@@ -0,0 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+using namespace std;
+
+TEST_CASE("find_largest returns the greater of two integers") {
+    CHECK(find_largest(6, 19) == 19);
+    CHECK(find_largest(6, 1) == 6);
+    CHECK(find_largest(22, 42) == 42);
+    CHECK(find_largest(42, 42) == 42);
+}
